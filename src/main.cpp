7878e8acb329474bae33aeefa6a740bc1f9035dd
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-09 01:39:26 +0500

    Servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
-#include <EEPROM.h>
 #include "../lib/button.h"
 #include <U8g2lib.h>
 #include <EncButton.h>
+#include "ESP32Servo.h"
+#include <cmath>
 
 #define PIN_LED_R 25
 #define PIN_LED_G 26
@@ -11,16 +12,19 @@
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, SCL, SDA);
 
 EncButton<EB_TICK, 34, 35, 32> encoder;
+Servo servo;
 
-volatile byte ledR = 255;
-volatile byte ledG = 170;
-volatile byte ledB = 20;
-volatile byte selectedLedPart = 0;
+volatile int servoPosition = 90;
+
+//volatile byte ledR = 255;
+//volatile byte ledG = 170;
+//volatile byte ledB = 20;
+//volatile byte selectedLedPart = 0;
 //volatile bool isSelectedLedPartBlink = false;
 
 TaskHandle_t updateScreenTaskHandle;
 TaskHandle_t encoderTaskHandle;
-TaskHandle_t selectedLedPartBlinkTaskHandle;
+//TaskHandle_t selectedLedPartBlinkTaskHandle;
 
 void updateScreen() {
     u8g2.firstPage();
@@ -31,16 +35,18 @@ void updateScreen() {
         u8g2.setDrawColor(1);
         u8g2.setFontMode(0);
 
-        u8g2.print("#");
+        u8g2.print(servoPosition);
 
-        u8g2.setDrawColor(selectedLedPart != 0 && selectedLedPart != 3);
-        u8g2.printf("%02X", ledR);
+//        u8g2.print("#");
 
-        u8g2.setDrawColor(selectedLedPart != 1 && selectedLedPart != 3);
-        u8g2.printf("%02X", ledG);
-
-        u8g2.setDrawColor(selectedLedPart != 2 && selectedLedPart != 3);
-        u8g2.printf("%02X", ledB);
+//        u8g2.setDrawColor(selectedLedPart != 0 && selectedLedPart != 3);
+//        u8g2.printf("%02X", ledR);
+//
+//        u8g2.setDrawColor(selectedLedPart != 1 && selectedLedPart != 3);
+//        u8g2.printf("%02X", ledG);
+//
+//        u8g2.setDrawColor(selectedLedPart != 2 && selectedLedPart != 3);
+//        u8g2.printf("%02X", ledB);
 //        if (isPressed) {
 //            u8g2.drawStr(0, 16 + 32, "BTN");
 //        }
@@ -51,18 +57,17 @@ void updateScreen() {
     } while (u8g2.nextPage());
 }
 
-void updateLed() {
-    analogWrite(PIN_LED_R, ledR);
-    analogWrite(PIN_LED_G, ledG);
-    analogWrite(PIN_LED_B, ledB);
-}
+//void updateLed() {
+//    analogWrite(PIN_LED_R, ledR);
+//    analogWrite(PIN_LED_G, ledG);
+//    analogWrite(PIN_LED_B, ledB);
+//}
 
 [[noreturn]]
 void updateScreenTask(void* pvParam) {
     while (true) {
         if (xTaskNotifyWait(0, 0, nullptr, portMAX_DELAY) == pdTRUE) {
             updateScreen();
-            updateLed();
         }
 
         vTaskDelay(1);
@@ -80,50 +85,38 @@ void encoderTask(void* pvParam) {
 
         bool isUpdated = false;
 
-        if (encoder.click()) {
-            isUpdated = true;
-            if (selectedLedPart < 3) {
-                selectedLedPart++;
-            } else {
-                selectedLedPart = 0;
-            }
-        }
-
         int step = 0;
+        int singleStep = 0;
         if (encoder.left()) {
             step = -5;
+            singleStep = -1;
         } else if (encoder.right()) {
             step = 5;
+            singleStep = 5;
         } else if (encoder.leftH()) {
-            step = -51;
+            step = -30;
+            singleStep = -1;
         } else if (encoder.rightH()) {
-            step = 51;
+            step = 30;
+            singleStep = 1;
         }
 
         if (step != 0) {
             isUpdated = true;
 
-            switch (selectedLedPart) {
-                case 0:
-                    ledR = constrain(ledR + step, 0, 255);
-                    break;
-                case 1:
-                    ledG = constrain(ledG + step, 0, 255);
-                    break;
-                case 2:
-                    ledB = constrain(ledB + step, 0, 255);
-                    break;
-                case 3:
-                    ledR = constrain(ledR + step, 0, 255);
-                    ledG = constrain(ledG + step, 0, 255);
-                    ledB = constrain(ledB + step, 0, 255);
-                    break;
-                default:
-                    break;
+            for (int i = 0; i < abs(step); i++) {
+                servoPosition = constrain(servoPosition + singleStep, 0, 180);
+
+                servo.write(servoPosition);
+                vTaskDelay(pdMS_TO_TICKS(2));
+
+                updateScreenAsync();
             }
         }
 
         if (isUpdated) {
+            //Serial.printf("#%02X%02X%02X\n", ledR, ledG, ledB);
+
             updateScreenAsync();
         }
 
@@ -144,15 +137,19 @@ void encoderTask(void* pvParam) {
 void setup() {
     Serial.begin(9600);
 
-    pinMode(PIN_LED_R, OUTPUT);
-    pinMode(PIN_LED_G, OUTPUT);
-    pinMode(PIN_LED_B, OUTPUT);
+//    pinMode(PIN_LED_R, OUTPUT);
+//    pinMode(PIN_LED_G, OUTPUT);
+//    pinMode(PIN_LED_B, OUTPUT);
+
+    servo.attach(25);
+
+    servo.write(servoPosition);
+    delay(300);
 
     u8g2.begin();
 
     xTaskCreate(encoderTask, "encoder", CONFIG_ESP_MAIN_TASK_STACK_SIZE, nullptr, 1, &encoderTaskHandle);
     xTaskCreate(updateScreenTask, "update-screen", CONFIG_ESP_MAIN_TASK_STACK_SIZE, nullptr, 1, &updateScreenTaskHandle);
-//    xTaskCreate(selectedLedPartBlinkTask, "selected-led-part-blink", CONFIG_ESP_MAIN_TASK_STACK_SIZE, nullptr, 1, &selectedLedPartBlinkTaskHandle);
 
     updateScreenAsync();
 }
